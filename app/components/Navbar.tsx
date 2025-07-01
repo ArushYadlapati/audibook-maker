ceb5fc1a0a0d4e01f858b09b45e214d3d83a0984
Khaiminh1902 <khaiminhnguyen1902@gmail.com>
2025-06-30 19:34:06 -0700

    added is active for navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,40 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  const isActive = (href: string) => {
+    return pathname === href || pathname.startsWith(href + "/");
+  };
   return (
     <div>
       <div className="flex items-center justify-between mt-10">
-        <div>
+        <div
+          className={`font-bold ${
+            isActive("/") ? "underline" : "hover:underline"
+          }`}
+        >
           <Link href="/">
             <h1 className="text-3xl font-bold ml-10">Audio Book Maker</h1>
           </Link>
         </div>
         <div className="flex gap-4 mr-10">
-          <Link href="/converter" className="hover:underline">
+          <Link
+            href="/converter"
+            className={`font-bold ${
+              isActive("/converter") ? "underline" : "hover:underline"
+            }`}
+          >
             <div className="text-lg">Converter</div>
           </Link>
-          <Link href="/library" className="hover:underline">
+          <Link
+            href="/library"
+            className={`font-bold ${
+              isActive("/library") ? "underline" : "hover:underline"
+            }`}
+          >
             <div className="text-lg">Library</div>
           </Link>
         </div>
